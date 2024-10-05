63e380449659065e091d0de3a359e77182eabd30
Nils Carlson <nilscarlson1997@gmail.com>
2024-10-04 23:23:48 -0400

    Time for slow get_at_pos

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,26 @@ const DAMPENING = 0.8
 const JUMP_FORCE = -800.0
 const MAX_FALL_SPEED = 1200.0
 
+func _input(event):
+	var scroll_value = 0
+	
+	if event.is_action_pressed("scroll_up"):
+		scroll_value = 1
+	if event.is_action_pressed("scroll_down"):
+		scroll_value = -1
+		
+	if scroll_value == 0:
+		return
+	
+	var camera = $Camera2D
+	# Adjust the zoom by the scroll value
+	var zoom_increment = 0.05 * scroll_value
+	camera.zoom += Vector2(zoom_increment, zoom_increment)
+	
+	# Clamp the zoom to the desired limits
+	camera.zoom.x = clamp(camera.zoom.x, 0.2, 1)
+	camera.zoom.y = clamp(camera.zoom.y, 0.2, 1)
+
 func _physics_process(delta):
 	# Apply gravity and cap the fall speed
 	if velocity.y < MAX_FALL_SPEED:
